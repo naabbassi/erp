3ed47ef285417117b71e98f47ea0d051aed3452d
Naser Abbasi <naser.abbasi1989@yahoo.com>
2015-03-31 11:29:16 +0300

    change the header menu

--- application/views/nav.php
+++ application/views/nav.php
@@ -16,29 +16,43 @@
                <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">فایل <span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><?= anchor('file/center', 'ناساندنی بنکەکان');?></li>
+                  <li><?= anchor('file/backup', 'وەرگرتنی فایل پشتیوان');?></li>
                   <li class="divider"></li>
-                  <li><?= anchor('file/customers','ناساندنی موشتەرێکان'); ?></li>
+                  <li><?= anchor('file/restore','گڕانەوە لە رێگەی فایلی پشتیوان'); ?></li>
+                  <li class="divider"></li>
+                  <li><?= anchor('file/user','بە کار هێنەران'); ?></li>
                   <li class="divider"></li>
                   <li><?= anchor('logout','دەرچۆن')?></li>
                 </ul>
               </li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">بەرهەم <span class="caret"></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">ناساندن <span class="caret"></a>
                  <ul class="dropdown-menu" role="menu">
-                  <li><?= anchor('products/product','ناساندنی بەرهەمەکان') ?></a></li>
-                  <li><?= anchor('products/unit','ناساندنی یەکەکان') ?></a></li>
+                  <li><?= anchor('define/storage',' کۆگا') ?></a></li>
                   <li class="divider"></li>
+                  <li><?= anchor('define/product',' کاڵاکان') ?></a></li>
+                  <li><?= anchor('define/unit',' یەکەی کاڵاکان') ?></a></li>
+                  <li class="divider"></li>
+                  <li><?= anchor('define/customer',' موشتەری') ?></a></li>
                 </ul>
               </li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">کارگێری<span class="caret"></span></a>
                 <ul class="dropdown-menu" role="menu">
-                  <li><?= anchor('operation/purchase','کڕین') ?></a></li>
+                  <li><?= anchor('operation/purchase','کڕینی کاڵا') ?></a></li>
                    <li class="divider"></li>
-                  <li><?= anchor('operation/sale','فرۆش') ?></a></li>
+                  <li><?= anchor('operation/sale','فرۆشی کاڵا') ?></a></li>
                    <li class="divider"></li>
                   <li><?= anchor('operation/payment','وەرگرتنی پارە') ?></a></li>
+                </ul>
+              </li>
+               <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">ژمێریاری<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><?= anchor('operation/purchase','هەژمارەکان') ?></a></li>
+                   <li class="divider"></li>
+                  <li><?= anchor('operation/sale','پاشکەوت کردنی بەڵگە') ?></a></li>
+                   <li class="divider"></li>
                 </ul>
               </li>
               <li class="dropdown">
