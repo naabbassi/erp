fb3479f0fed74f4fc56d7f3a6dd8b06a3ae7fd63
Naser Abbasi <naser.abbasi447@gmail.com>
2016-02-01 22:21:07 +0100

    another commit by amanj

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 $config['base_url']	= 'http://localhost/ERP';
-
+$config['currency_symbol'] = 'دینار';
 /*
 |--------------------------------------------------------------------------
 | Index File
